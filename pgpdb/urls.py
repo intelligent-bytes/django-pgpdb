7dacb66043ae05bb786e1232a610b6dbd98d70e0
Jonathan Barratt <jonathan@intelligent-bytes.com>
2020-11-24 22:25:20 +0700

    update url config for django 2 compatibility

--- pgpdb/urls.py
+++ pgpdb/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r"^$", views.index, name="index"),
-    url(r"^add$", views.add, name="add"),
-    url(r"^lookup$", views.lookup, name="lookup"),
+    path("", views.index, name="index"),
+    path("add/", views.add, name="add"),
+    path("lookup/", views.lookup, name="lookup"),
 ]
